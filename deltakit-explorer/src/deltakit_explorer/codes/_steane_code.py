28ea38853eebbb8e75da40832cd47e5512147633
Hrushikesh Patil <hrushikeshpatil890@gmail.com>
2025-11-23 13:24:35 -0500

    Create Steane code

--- deltakit-explorer/src/deltakit_explorer/codes/_steane_code.py
+++ deltakit-explorer/src/deltakit_explorer/codes/_steane_code.py
@@ -12,3 +12,17 @@ from deltakit_circuit._qubit_identifiers import PauliGate
 from deltakit_circuit.gates import PauliBasis
 from deltakit_explorer.codes._css._css_code import CSSCode
 from deltakit_explorer.codes._stabiliser import Stabiliser
+
+class SteaneCode(CSSCode):
+
+    def __init__(self, distance: int, stabiliser_type: PauliBasis = PauliBasis.Z, use_ancilla_qubits: bool = True, use_looping_stabiliser: bool = False, default_schedule: bool = True, odd_data_qubit_coords: bool = False):
+        self.distance = distance
+        self.stabiliser_type = stabiliser_type
+        self.use_ancilla_qubits = use_ancilla_qubits
+        self.use_looping_stabiliser = use_looping_stabiliser
+        self.default_schedule = default_schedule
+        self.odd_data_qubit_coords = odd_data_qubit_coords
+
+        super().__init__(use_ancilla_qubits=use_ancilla_qubits, use_looping_stabiliser=use_looping_stabiliser, default_schedule=default_schedule, odd_data_qubit_coords=odd_data_qubit_coords)
+
+    def _calculate_data_qubits(self) -> Set[Qubit]:
